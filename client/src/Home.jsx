7a1974422fee9638fbedbd9a34e3b5dd8d3d1794
Shubham Sawant <sawantsb27@gmail.com>
2025-09-04 19:09:00 +0530

    Complete Signup Flow

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
 import './App.css'
 const Home = () => {
+  const auth=localStorage.getItem('user')
   return (
     <>
 
@@ -10,9 +11,9 @@ const Home = () => {
         <Link to="/Product">Product</Link>
         <Link to="/AddProduct">Add Product</Link>
         <Link to="/UpdateProduct">Update Product</Link>
-        <Link to="/Logout">Log out</Link>
+        
         <Link to="/Profile">Profile</Link>
-        <Link to="/Signup">Sign-Up</Link>
+        {auth?<Link to="/Logout">Log out</Link>: <Link to="/Signup">Sign-Up</Link>}
         </div>
       <h1>Landing Page / Home </h1>
     </>
--- client/src/Routes/NavRoutes.jsx
+++ client/src/Routes/NavRoutes.jsx
@@ -7,17 +7,21 @@ import Logout from './Logout'
 import Profile from './Profile'
 import Home from '../Home'
 import Signup from './Signup'
+import PrivateComponent from './PrivateComponent'
 const NavRoutes = () => {
   return (
     <>
     <BrowserRouter>
     <Routes>
+        //Private Component
+        <Route element={<PrivateComponent/>}>
         <Route path='/'element={<Home/>}/>
         <Route path='/Product' element={<Product/>}/>
         <Route path="/AddProduct" element={<AddProduct/>}/>
         <Route path="/UpdateProduct" element={<UpdateProduct/>}/>
         <Route path="/Logout" element={<Logout/>}/>
         <Route path="/Profile" element={<Profile/>}/>
+        </Route>
         <Route path='/Signup' element={<Signup/>}/>
     </Routes>
     </BrowserRouter>
